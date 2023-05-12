03f64bc0b426fae9c9ba0025d796a1c03b1018fc
Ana211202 <al298422@edu.uaa.mx>
2023-05-12 10:29:36 -0600

    componente reservas

--- reserva/src/app/app.module.ts
+++ reserva/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GaleriaComponent
+    GaleriaComponent,
+    ReservasComponent
   ],
   imports: [
     BrowserModule, FormsModule
